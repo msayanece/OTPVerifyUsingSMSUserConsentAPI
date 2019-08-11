6ed6801fde9a80a0704ee15243533d146ed8cc1f
msayanece <msayanece@gmail.com>
2019-08-12 00:31:32 +0530

    Add: All the logic of SMS User Consent API and add the BroadcastReceiver and update the onActivityResult method for getting the message from User consent
    Note 1: Parsing OTP from the Message content and calling server API with that OTP (to verify) is out of scope in this project.
    
    Note 2: Code for - send request to server for sending OTP is not done yet

--- app/src/main/java/com/sayan/otpverifyusingsmsuserconsentapi/MainActivity.kt
+++ app/src/main/java/com/sayan/otpverifyusingsmsuserconsentapi/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.sayan.otpverifyusingsmsuserconsentapi
 
 import android.app.Activity
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -10,9 +9,13 @@ import android.widget.Toast
 import com.google.android.gms.auth.api.credentials.Credential
 import com.google.android.gms.auth.api.credentials.Credentials
 import com.google.android.gms.auth.api.credentials.HintRequest
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.Status
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val CREDENTIAL_PICKER_REQUEST = 1  // Set to an unused request code
+private const val SMS_CONSENT_REQUEST = 2  // Set to an unused request code
 
 /**
  * Google documentation
@@ -22,7 +25,34 @@ private const val CREDENTIAL_PICKER_REQUEST = 1  // Set to an unused request cod
  */
 class MainActivity : AppCompatActivity(){
 
-    var userPhoneNumber: String = ""
+
+    private val smsVerificationReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
+                val extras = intent.extras
+                val smsRetrieverStatus = extras?.get(SmsRetriever.EXTRA_STATUS) as Status
+
+                when (smsRetrieverStatus.statusCode) {
+                    CommonStatusCodes.SUCCESS -> {
+                        // Get consent intent
+                        val consentIntent = extras.getParcelable<Intent>(SmsRetriever.EXTRA_CONSENT_INTENT)
+                        try {
+                            // Start activity to show consent dialog to user, activity must be started in
+                            // 5 minutes, otherwise you'll receive another TIMEOUT intent
+                            startActivityForResult(consentIntent, SMS_CONSENT_REQUEST)
+                        } catch (e: ActivityNotFoundException) {
+                            // Handle the exception ...
+                            e.printStackTrace()
+                        }
+                    }
+                    CommonStatusCodes.TIMEOUT -> {
+                        // Time out occurred, handle the error.
+                        println("SMS user consent timeout.")
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +64,28 @@ class MainActivity : AppCompatActivity(){
             val phoneNumber: String = phoneEditText.text.toString()
             //Use regex ^[6-9]\d{9}$ for indian mobile number checking
             if (phoneNumber.matches(Regex("^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[6789]\\d{9}\$"))){
-                verifyPhoneNumber()
+                verifyPhoneNumber(phoneNumber)
             }else{
                 showToast("Phone number is invalid")
             }
-
         }
+
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        registerReceiver(smsVerificationReceiver, intentFilter)
     }
 
-    private fun verifyPhoneNumber() {
-        showToast("Phone number verified successfully")
+    private fun verifyPhoneNumber(phoneNumber: String) {
+
+        startListeningToOTPSMS()
+    }
+
+    /**
+     * Start listening for SMS User Consent broadcasts from smsGatewaySenderPhoneNumber.
+     * The Task<Void> will be successful if SmsRetriever was able to start SMS User Consent,
+     * and will error if there was an error starting.
+     */
+    private fun startListeningToOTPSMS() {
+        val task = SmsRetriever.getClient(this).startSmsUserConsent(null /*or smsGatewaySenderPhoneNumber*/)
     }
 
     // Construct a request for phone numbers and show the picker
@@ -70,14 +112,35 @@ class MainActivity : AppCompatActivity(){
             CREDENTIAL_PICKER_REQUEST ->
                 // Obtain the phone number from the result of hint picker
                 if (resultCode == Activity.RESULT_OK && data != null) {
+                    //fetch the phone number selected and set the phone number to the edit text
                     val credential = data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)
                     println("Phone number: ${credential.id}")
                     phoneEditText.setText(credential.id)
+                    //call verifyButton onClick method after a delay
                     Handler().postDelayed({
                         verifyButton.callOnClick()
                     },250)
                 }
+            SMS_CONSENT_REQUEST ->
+                // Obtain the phone number from the result
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // Get SMS message content
+                    val message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
+                    parseOTPAndCompleteVerification(message) // define this function
+                } else {
+                    println("Consent denied. User can type OTP manually.")
+                }
         }
+    }
+
+    /**
+     * Extract one-time code from the message and
+     * complete verification `message` contains the entire text of the SMS message,
+     * so you will need to parse the string.
+     */
+    private fun parseOTPAndCompleteVerification(message: String?){
+        // send one time code to the server after parsing the OTP from the message String
+        showToast("Phone number verified successfully!")
     }
 
     fun Context.showToast(message: String){
